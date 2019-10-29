9abfad2488c3863c2879849c1cc31442e626ebc4
Barış Kaan Bayram <bariskaanb@gmail.com>
2019-10-29 16:58:12 +0300

    Defining classes for first scenario with Database v1.0

--- src/main/java/com/ab/buscardsystem/Starter.java
+++ src/main/java/com/ab/buscardsystem/Starter.java
@@ -1,12 +1,34 @@
 package com.ab.buscardsystem;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Starter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         WholeSystem start = new WholeSystem();
         System.out.println("Bütün sistem oluşturuldu: " + start);
         start.start();
+
+        String find = "Allah";
+        String query1 = "INSERT INTO Card (Name, Surname, Bakiye) VALUES ('Allah', 'ü-teala', 999)";
+        SqliteDB sqliteDB = new SqliteDB();
+
+
+        String query2 = "DELETE FROM Card WHERE Name = ?";
+        PreparedStatement preparedStatement = sqliteDB.connection.prepareStatement(query2);
+        preparedStatement.setString(1,find);
+        preparedStatement.executeUpdate();
+
+        sqliteDB.listCards();
+
+        sqliteDB.executeQuery(query1);
+
+        sqliteDB.listCards();
+
+
+        sqliteDB.closeDB();
 
     }
 }
